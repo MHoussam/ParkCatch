1f538f582b51ef17ef6266a4b804f3a581ea9359
MHoussam <mohammadosta1999@gmail.com>
2023-09-08 22:44:41 +0300

    Server <> Created the getSpots, register, getParkings, & reserveSpot APIs for the Client.

--- ser/app/Http/Controllers/ClientController.php
+++ ser/app/Http/Controllers/ClientController.php
@@ -2,9 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Parking;
+use App\Models\Reservation;
+use App\Models\Spot;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    //
+    public function getParkings() {
+        $parkings = Parking::all();
+
+        return response()->json([
+            'status' => 'Error',
+            'data' => $parkings
+        ]);
+    }
+
+    public function reserveSpot(Request $request) {
+        $reserve = new Reservation;
+        $reserve->user_id = $request->user_id;
+        $reserve->parking_id = $request->parking_id;
+        $reserve->spot_id = $request->spot_id;
+        $reserve->duration = $request->duration;
+        $reserve->total = $request->total;
+        $reserve->valid = true;
+        $reserve->plate_number = $request->plate_number;
+        $reserve->phone_number = $request->phone_number;
+        $reserve->time_reserved = $request->time_reserved;
+        $reserve->date_reserved = $request->date_reserved;
+
+        $spot = Spot::where('id', $request->spot_id)
+                    ->where('parking_id', $request->parking_id)
+                    ->first();
+
+        $reserved = Reservation::where('spot_id', $request->spot_id)
+                               ->where('valid', true)
+                               ->first();
+
+        if($spot != null && $reserved == null) {
+            $reserve->save();
+
+            return response()->json([
+                'status' => 'Success',
+                'data' => 'Spot has been reserved.'
+            ]);
+        }
+        else {
+            return response()->json([
+                'status' => 'Error',
+                'data' => 'The spot is already reserved.'
+            ]);
+        }
+    }
 }
--- ser/routes/api.php
+++ ser/routes/api.php
@@ -21,14 +21,21 @@ Route::group(["middleware"=>"auth:api"],function(){
         Route::post('validity', [SupervisorController::class, 'checkValidity']);
         Route::post('spots', [SupervisorController::class, 'getSpots']);
         Route::post('availability', [SupervisorController::class, 'changeAvailability']);
-        Route::post('parkingDetails', [AdminController::class, 'changeAvailability']);
+        Route::post('parkingDetails', [AdminController::class, 'changeParkingDetails']);
         Route::post('ban', [AdminController::class, 'banCustomer']);
         Route::post('addSupervisor', [AuthController::class, 'register']);
         Route::post('removeSupervisor', [AdminController::class, 'removeSupervisor']);
+    });
+
+    Route::group(["middleware"=>"auth.client"],function(){
+        Route::post('spots', [SupervisorController::class, 'getSpots']);
+        Route::post('register', [AuthController::class, 'register']);
+        Route::get('parkings', [ClientController::class, 'getParkings']);
+        Route::post('reserve', [ClientController::class, 'reserveSpot']);
     });
 });
 
 Route::get("unauthorized", [AuthController::class, "unauthorized"])->name("unauthorized");
 Route::post('login',[AuthController::class,'login']);
-Route::post('register',[AuthController::class,'register']);
+//Route::post('register',[AuthController::class,'register']);
 Route::get('logout',[AuthController::class,'logout']);
