218a6b0bffb121c687dda134e8704b5ae67647cf
MHoussam <mohammadosta1999@gmail.com>
2023-09-14 22:49:47 +0300

    Server <> Added the Route of the API availableSpots.

--- ser/routes/api.php
+++ ser/routes/api.php
@@ -30,6 +30,7 @@ Route::group(["middleware"=>"auth:api"],function(){
         Route::post('spots', [SupervisorController::class, 'getSpots']);
         Route::post('register', [AuthController::class, 'register']);
         Route::get('parkings', [ClientController::class, 'getParkings']);
+        Route::post('available', [ClientController::class, 'availableSpots']);
         Route::post('reserve', [ClientController::class, 'reserveSpot']);
         Route::post('reservations', [ClientController::class, 'getReservations']);
         Route::post('changeInfo', [ClientController::class, 'changeAccountInfo']);
