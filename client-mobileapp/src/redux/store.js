18e0ee9471f051c521a896281249debcaae31387
MHoussam <mohammadosta1999@gmail.com>
2023-09-14 23:20:37 +0300

    Client-MobileApp <> Created the Reducer for the Distance Calculated.

--- client-mobileapp/src/redux/store.js
+++ client-mobileapp/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
 import locationReducer from './location/locationSlice';
 import parkingReducer from './parking/parkingSlice';
+import distanceReducer from './distance/distanceSlice';
 
 const store = configureStore({
     reducer: {
         user: userReducer,
         location: locationReducer,
         parking: parkingReducer,
+        distance: distanceReducer,
     },
 });
 
