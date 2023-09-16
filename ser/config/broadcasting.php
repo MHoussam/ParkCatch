1563b01ec59be666e029fb8144f7c470c0efc217
MHoussam <mohammadosta1999@gmail.com>
2023-09-16 15:54:39 +0300

    Server <> Created ProcessPodcast.php File in the Jobs Folder.

--- ser/config/broadcasting.php
+++ ser/config/broadcasting.php
@@ -36,7 +36,7 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'cluster' => 'ap2',
                 'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'http',
