cab3dc7b3f059f90671f8cd8f53d631ccf88e71f
MHoussam <mohammadosta1999@gmail.com>
2023-09-17 16:50:08 +0300

    Client-MobileApp <> Created the Directions Screen.

---
+++ client-mobileapp/src/screens/directions/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './styles';
+import Map from '../../components/ui/map';
+
+const Directions = () => {
+  return (
+    <View>
+        <Map />
+    </View>
+  )
+}
+
+export default Directions;
