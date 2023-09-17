59e6349b88c9fb76960e3de1dab45b146767bba5
MHoussam <mohammadosta1999@gmail.com>
2023-09-17 14:31:23 +0300

    Client-MobileApp <> Created the Style File for ReservationInfo Screen.

--- client-mobileapp/src/screens/reservationInfo/index.jsx
+++ client-mobileapp/src/screens/reservationInfo/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { View } from 'react-native'
 import Header from '../../components/ui/header';
+import Footer from '../../components/ui/footer';
+import styles from './styles';
 
 const ReservationInfo = () => {
   return (
-    <View>
+    <View style={styles.container}>
         <Header />
         <Footer />
     </View>
