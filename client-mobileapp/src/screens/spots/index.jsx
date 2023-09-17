6e16292e1307c95e9bbb30bfab836055869e918e
MHoussam <mohammadosta1999@gmail.com>
2023-09-17 04:36:11 +0300

    Client-MobileApp <> Added the Navigation Method when Selected a Slot & Pressing Next.

--- client-mobileapp/src/screens/spots/index.jsx
+++ client-mobileapp/src/screens/spots/index.jsx
@@ -4,14 +4,24 @@ import { Text, TouchableOpacity, View } from 'react-native';
 import Slots from '../../components/ui/slots';
 import Header from '../../components/ui/header';
 import Footer from '../../components/ui/footer';
+import { useSelector } from 'react-redux';
 
 const Spots = () => {
+  const navigation = useNavigation();
+  const selectedSlot = useSelector((stat) => state.slots);
+
+  const navigateToReservationInfo = () => {
+    if(selectedSlot !== null) {
+      navigation.navigate('ReservationInfo');
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Header />
       <Text style={styles.title}>Select a Spot</Text>
       <Slots />
-      <TouchableOpacity style={styles.nextButton}>
+      <TouchableOpacity style={styles.nextButton} onPress={navigateToReservationInfo}>
         <Text style={styles.buttonText}>Next</Text>
       </TouchableOpacity>
       <Footer />
