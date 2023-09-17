167836f36515bcb89a925edee10efdac9a443821
MHoussam <mohammadosta1999@gmail.com>
2023-09-17 16:46:11 +0300

    Client-MobileApp <> Created the Going Back to the Previous Screen Navigation.

--- client-mobileapp/src/components/ui/header/index.jsx
+++ client-mobileapp/src/components/ui/header/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import styles from './styles';
 import { Image, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 const Header = ({ ScreenName, ParkingName }) => {
+  const navigation = useNavigation();
+
+  const back = () => {
+    navigation.pop();
+  }
+
   return (
     <View style={styles.container}>
         <View style={styles.content}>
-          <Image source={require('../../../../assets/images/back.png')} />
+          <TouchableOpacity onPress={back} >
+            <Image source={require('../../../../assets/images/back.png')} />
+          </TouchableOpacity>
           <View style={styles.text}>
             <Text style={styles.title}>{ScreenName}</Text>
             <Text style={styles.subtitle}>{ParkingName}</Text>
