0d081d5cebf0c3a6e30d47cff77388427aa86096
MHoussam <mohammadosta1999@gmail.com>
2023-09-17 17:12:38 +0300

    Client-MobileApp <> Created the Style File for the Component Button.

---
+++ client-mobileapp/src/components/base/button/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    nextButton: {
+      width: '83%',
+      height: 35,
+      backgroundColor: '#FECA0E',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 20,
+      position: 'absolute',
+      bottom: 88,
+    },
+    buttonText: {
+      color: '#000',
+      fontWeight: 'bold',
+      fontSize: 17,
+    }
+});
+
+export default styles;
