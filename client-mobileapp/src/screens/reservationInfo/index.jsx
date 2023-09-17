6cc203b7f2a31c517e3871e1b764861579f63961
MHoussam <mohammadosta1999@gmail.com>
2023-09-17 17:05:39 +0300

    Client-MobileApp <> Added the Button in Reservation Info Screen to Navigate to the Next Screen.

--- client-mobileapp/src/screens/reservationInfo/index.jsx
+++ client-mobileapp/src/screens/reservationInfo/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 import Header from '../../components/ui/header';
 import Footer from '../../components/ui/footer';
 import styles from './styles';
 import { useSelector } from 'react-redux';
+import Map from '../../components/ui/map';
 
 const ReservationInfo = () => {
     const selectedParking = useSelector((state) => state.selectedParking);
@@ -11,6 +12,10 @@ const ReservationInfo = () => {
   return (
     <View style={styles.container}>
         <Header ScreenName={'Reservation Info'} ParkingName={selectedParking.name} />
+        {/* <Map /> */}
+        <TouchableOpacity style={styles.nextButton} >
+          <Text style={styles.buttonText}>Proceed to Payment</Text>
+        </TouchableOpacity>
         <Footer />
     </View>
   )
