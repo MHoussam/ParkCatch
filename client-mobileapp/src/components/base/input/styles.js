f056a5625276d196ccca59f5fa972b9d02fa54e0
MHoussam <mohammadosta1999@gmail.com>
2023-09-19 15:44:40 +0300

    Client-MobileApp <> Created the Style File for the Input Component.

---
+++ client-mobileapp/src/components/base/input/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 8,
+        paddingHorizontal: 10,
+        fontSize: 16,
+        marginBottom: 10,
+        color: 'black',
+      },
+});
+
+export default styles;
