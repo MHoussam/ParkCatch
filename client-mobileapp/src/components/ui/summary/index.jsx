ca39c576aeadf8609f102709227f7a33d8fc6721
MHoussam <mohammadosta1999@gmail.com>
2023-09-19 18:37:12 +0300

    Client-MobileAPp <> Fixed the Display of the Information in the Summary to Make it Retrieve the Values from the Reservation Reducer.

--- client-mobileapp/src/components/ui/summary/index.jsx
+++ client-mobileapp/src/components/ui/summary/index.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { Text, View } from 'react-native'
 import Info from '../../base/info'
 import styles from './styles'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { setReservation } from '../../../redux/reservation/reservationSlice'
 
 const Summary = () => {
+    const dispatch = useDispatch();
     const user = useSelector((state) => state.user);
     const selectedParking = useSelector((state) => state.selectedParking);
     const selectedSlot = useSelector((state) => state.selectedSlot);
@@ -13,7 +15,7 @@ const Summary = () => {
     console.log(reservation)
     console.log(reservation.duration)
 
-// console.log(user)
+    dispatch(setReservation({client: user.firstname + ' ' + user.lastname, parking: selectedParking.name, location: selectedParking.address, spotNumber: selectedSlot.name, total: parseInt(reservation.duration) * 2}));
 
   return (
     <View style={styles.container}>
@@ -21,7 +23,7 @@ const Summary = () => {
         <View style={styles.row}>
             <View style={styles.leftContent}>
                 <Info styleText={styles.subtitle} text={'Client'} />
-                <Info styleText={styles.info} text={user.firstname + ' ' + user.lastname} />
+                <Info styleText={styles.info} text={reservation.client} />
             </View>
             <View>
                 <Info styleText={styles.subtitle} text={'Phone Number'} />
@@ -32,11 +34,11 @@ const Summary = () => {
         <View style={styles.row}>
             <View style={styles.leftContent}>
                 <Info styleText={styles.subtitle} text={'Parking'} />
-                <Info styleText={styles.info} text={selectedParking.name} />
+                <Info styleText={styles.info} text={reservation.parking} />
             </View>
             <View>
                 <Info styleText={styles.subtitle} text={'Location'} />
-                <Info styleText={styles.info} text={selectedParking.address} />
+                <Info styleText={styles.info} text={reservation.location} />
             </View>
         </View>
         
@@ -47,7 +49,7 @@ const Summary = () => {
             </View>
             <View>
                 <Info styleText={styles.subtitle} text={'Spot Number'} />
-                <Info styleText={styles.info} text={selectedSlot.name} />
+                <Info styleText={styles.info} text={reservation.spotName} />
             </View>
         </View>
         <View style={styles.row}>
@@ -57,7 +59,7 @@ const Summary = () => {
             </View>
             <View>
                 <Info styleText={styles.subtitle} text={'Total'} />
-                <Info styleText={styles.info} text={selectedSlot.name} />
+                <Info styleText={styles.info} text={reservation.total} />
             </View>
         </View>
     </View>
