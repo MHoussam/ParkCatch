34721e58b2bd1ec66cb939babc5f78f9bd89b864
MHoussam <mohammadosta1999@gmail.com>
2023-09-19 16:09:58 +0300

    Client-MobileApp <> Added the Text fr the Component Summary.

--- client-mobileapp/src/components/ui/summary/index.jsx
+++ client-mobileapp/src/components/ui/summary/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
+import Info from '../../base/info'
 
 const Summary = () => {
   return (
     <View style={{flex: 1}}>
-        
+        <Text>Summary</Text>
+        <Info styleText={{color:'grey'}} text={'Client'} />
     </View>
   )
 }
