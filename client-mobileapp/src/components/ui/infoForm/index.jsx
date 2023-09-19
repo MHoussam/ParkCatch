0e84f23cd859072e42d3a53fe7548670023673fe
MHoussam <mohammadosta1999@gmail.com>
2023-09-19 15:51:07 +0300

    Client-MobileApp <> Added the Input Component to the InfoForm Component.

--- client-mobileapp/src/components/ui/infoForm/index.jsx
+++ client-mobileapp/src/components/ui/infoForm/index.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { View } from 'react-native'
 import Duration from '../../base/duration';
 import styles from './styles'
+import Input from '../../base/input';
 
 const InfoForm = () => {
   return (
     <View style={styles.container}>
         <Duration styleText={styles.title}/>
+        <Input styleText={styles.title} text={'License Plate Number'}/>
     </View>
   )
 }
