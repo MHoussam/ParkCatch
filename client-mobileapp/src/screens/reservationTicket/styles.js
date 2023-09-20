0cd96c25e1b07cb1eea4b0a434c3494a2b5e6f7e
MHoussam <mohammadosta1999@gmail.com>
2023-09-20 11:47:40 +0300

    Client-MobileApp <> Created the Style File for ReservationTicket Screen.

---
+++ client-mobileapp/src/screens/reservationTicket/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        paddingTop: 10,
+        marginLeft: 0,
+        marginRight: 0,
+        width: '100%',
+        backgroundColor: '#FFFFFF'
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginTop: 10,
+    },
+});
+
+export default styles;
