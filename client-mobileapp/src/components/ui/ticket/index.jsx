aab5ec5f57b4312e735b1426e60968c7d24a7581
MHoussam <mohammadosta1999@gmail.com>
2023-09-20 12:13:25 +0300

    Client-MobileApp <> Added the Image to the Ticket Component.

--- client-mobileapp/src/components/ui/ticket/index.jsx
+++ client-mobileapp/src/components/ui/ticket/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Image, View } from 'react-native'
 import styles from './styles'
 
 const Ticket = () => {
   return (
     <View style={styles.container}>
-        
+        <View style={styles.innerContainer}>
+            <Image
+                style={{resizeMode: 'cover',
+                width: 200, 
+                height: 200}}
+                source={{uri: `http://127.0.0.1:8000/images/parkings/saidamall.png`}}
+            /> 
+        </View>
     </View>
   )
 }
--- client-mobileapp/src/components/ui/ticket/styles.js
+++ client-mobileapp/src/components/ui/ticket/styles.js
@@ -2,8 +2,13 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
-        borderWidth: 1,
-        flex: 1,
+        // flex: 1,
+        width: '90%',
+        height: '64%',
+        marginTop: 28,
+        // alignContent: 'center',
+        borderRadius: 25,
+        backgroundColor: '#FECA0E',
     },
 })
 
