1226eeca0a658a0f5d3d0299ec317baa9b1e7638
MHoussam <mohammadosta1999@gmail.com>
2023-09-20 11:48:52 +0300

    Client-MobileApp <> Added Styling for the Container in ReservationTicket Screen.

--- client-mobileapp/src/screens/reservationTicket/index.jsx
+++ client-mobileapp/src/screens/reservationTicket/index.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { View } from 'react-native'
 import Header from '../../components/ui/header'
 import Footer from '../../components/ui/footer'
+import styles from './styles'
 
 const ReservationTicket = () => {
   return (
-    <View>
+    <View style={styles.container}>
         <Header />
         
         <Footer />
