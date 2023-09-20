2c958c2e3b09309427362d795656a2efb99e273b
MHoussam <mohammadosta1999@gmail.com>
2023-09-20 10:04:18 +0300

    Client-MobileApp <> Added a UserToken State Hook & checkUserToken Function in App.js to Check if the User has a Token or Not.

--- client-mobileapp/App.js
+++ client-mobileapp/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
@@ -9,15 +9,31 @@ import Home from './src/screens/home';
 import Spots from './src/screens/spots';
 import ReservationInfo from './src/screens/reservationInfo';
 import Directions from './src/screens/directions';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [userToken, setUserToken] = useState(null);
+  
+  useEffect(() => {
+    const checkUserToken = async () => {
+      const token = await AsyncStorage.getItem('userToken');
+      if (token !== null) {
+        setUserToken(token);
+      }
+    };
+    
+    checkUserToken();
+  }, []);
+
   return (
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
+          {if(userToken !=== null){
           <Stack.Screen name="Login" component={Login} />
+          }}
           {/* <Stack.Screen name="Signup" component={Signup} /> */}
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="Spots" component={Spots} />
