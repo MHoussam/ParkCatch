cabcb9da3f1baeef13229d07b29f55337a14184f
MHoussam <mohammadosta1999@gmail.com>
2023-09-21 11:01:10 +0300

    Client-MobileApp <> Added the Image to the Reservation Card Component.

--- client-mobileapp/src/components/base/reservationCard/index.jsx
+++ client-mobileapp/src/components/base/reservationCard/index.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { View } from "react-native";
 
 const ReservationCard = () => {
   return (
-    <div>ReservationCard</div>
-  )
-}
+    <View>
+      <TouchableOpacity style={styles.card} onPress={navigateToSpots}>
+        <Image
+          style={styles.parkingPhoto}
+          source={{
+            uri: `http://127.0.0.1:8000/images/parkings/${selectedParking.photo}`,
+          }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-export default ReservationCard
+export default ReservationCard;
