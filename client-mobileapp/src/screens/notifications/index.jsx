f4ab60c5e95d69bdc8ec761972dfaa2579a07d10
MHoussam <mohammadosta1999@gmail.com>
2023-09-21 11:57:28 +0300

    Client-MobileApp <> Added the Header to the Notifications Screen.

--- client-mobileapp/src/screens/notifications/index.jsx
+++ client-mobileapp/src/screens/notifications/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import registerNNPushToken from 'native-notify';
 import Footer from '../../components/ui/footer';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
+import Header from '../../components/ui/header';
 
 const Notifications = () => {
   return (
     <View style={{flex:1}}>
+      <Header ScreenName={'Notifications'} />
+      <View style={{flex:1}}>
+        <Text></Text>
+      </View>
       <Footer />
     </View>
   )
