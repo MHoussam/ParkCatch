79a965873001ee44f900e84a0aa330e224d5caed
MHoussam <mohammadosta1999@gmail.com>
2023-09-21 11:07:26 +0300

    Client-MobileApp <> Added the Icon for th Parking Distance to the Reservation Card Component.

--- client-mobileapp/src/components/base/reservationCard/index.jsx
+++ client-mobileapp/src/components/base/reservationCard/index.jsx
@@ -1,45 +1,37 @@
-import React from "react";
-import { View } from "react-native";
+import React from 'react'
+import { View } from 'react-native'
 
 const ReservationCard = () => {
   return (
     <View>
-      <TouchableOpacity style={styles.card} onPress={navigateToSpots}>
-        <Image
-          style={styles.parkingPhoto}
-          source={{
-            uri: `http://127.0.0.1:8000/images/parkings/${selectedParking.photo}`,
-          }}
-        />
-        <View>
-          <View style={styles.cardInfo}>
-            <Text style={[styles.bold, styles.size16]}>
-              {selectedParking.name}
-            </Text>
-            <Text style={[styles.bold, styles.size13, styles.parkingAddress]}>
-              {selectedParking.address}
-            </Text>
-            <View style={styles.cardInfoRow}>
-              <View style={styles.cardInfoRow}>
-                <Image
-                  source={require("../../../../assets/images/spots.png")}
-                />
-                <Text style={[styles.semiBold, styles.size13]}>
-                  {" "}
-                  {availableNumber} Spots
-                </Text>
+        <TouchableOpacity style={styles.card} onPress={navigateToSpots}>
+              <Image
+                style={styles.parkingPhoto}
+                source={{
+                  uri: `http://127.0.0.1:8000/images/parkings/${selectedParking.photo}`,
+                }}
+              />
+              <View >
+                <View style={styles.cardInfo} >
+                  <Text style={[styles.bold, styles.size16]}>{selectedParking.name}</Text>
+                  <Text style={[styles.bold, styles.size13, styles.parkingAddress]}>{selectedParking.address}</Text>
+                  <View style={styles.cardInfoRow} >
+                    <View style={styles.cardInfoRow}>
+                      <Image source={require('../../../../assets/images/spots.png')} />
+                      <Text style={[styles.semiBold, styles.size13]}> {availableNumber} Spots</Text>
+                    </View>
+                    <View style={styles.cardInfoRow}>
+                      <Image source={require('../../../../assets/images/distance.png')} />
+                      <Text style={[styles.semiBold, styles.size13]}>
+                        {calculatedDistance}m
+                      </Text>
+                    </View>
+                  </View>
+                </View>
               </View>
-              <View style={styles.cardInfoRow}>
-                <Text style={[styles.semiBold, styles.size13]}>
-                  {calculatedDistance}m
-                </Text>
-              </View>
-            </View>
-          </View>
-        </View>
-      </TouchableOpacity>
+              </TouchableOpacity>
     </View>
-  );
-};
+  )
+}
 
-export default ReservationCard;
+export default ReservationCard
