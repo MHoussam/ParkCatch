5e08b685100d375cc50c387efc8d494fd709b1ad
MHoussam <mohammadosta1999@gmail.com>
2023-09-23 00:42:39 +0300

    Client-MobileApp <> Commented the Functions Responsible for Sending the Notifications.

--- client-mobileapp/App.js
+++ client-mobileapp/App.js
@@ -18,7 +18,9 @@ import * as Notifications from "expo-notifications";
 import Notification from "./src/components/ui/notification";
 import Reservations from "./src/screens/reservations";
 import Settings from "./src/screens/settings";
-import { Image } from "react-native";
+import { Alert, Image, Text } from "react-native";
+import {PermissionsAndroid} from 'react-native';
+  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -42,6 +44,58 @@ export default function App() {
     console.log('this is the token ' + token);
     setUserToken(token);
   }
+
+  
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+// useEffect(() => {
+//   if(requestUserPermission) {
+//     messaging().getToken().then(token => {
+//       console.log('yesssss: '+ token)
+//     })
+//   } else {
+//     console.log("Failed token status ", authStatus)
+//   }
+
+//   messaging()
+//       .getInitialNotification()
+//       .then(async (remoteMessage) => {
+//         if (remoteMessage) {
+//           console.log(
+//             'Notification caused app to open from quit state:',
+//             remoteMessage.notification,
+//           );
+//         }
+//       });
+
+
+//       messaging().onNotificationOpenedApp(async (remoteMessage) => {
+//         console.log(
+//           'Notification caused app to open from background state:',
+//           remoteMessage.notification,
+//         );
+//       });
+
+//       messaging().setBackgroundMessageHandler(async remoteMessage => {
+//         console.log('Message handled in the background!', remoteMessage);
+//       });
+
+//       const unsubscribe = messaging().onMessage(async remoteMessage => {
+//         Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+//       });
+  
+//       return unsubscribe;
+// }, [])
+
 
   useEffect(() => {
     registerForPushNotificationsAsync();
