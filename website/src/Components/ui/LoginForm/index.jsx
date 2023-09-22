7169b2389f2f5659e0d3deeb8127512f57dad9e5
MHoussam <mohammadosta1999@gmail.com>
2023-09-23 02:37:41 +0300

    Client-Website <> Added the Image Component to the LoginForm Component.

--- website/src/Components/ui/LoginForm/index.jsx
+++ website/src/Components/ui/LoginForm/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Input from '../../base/input'
 import './styles.css'
+import Image from '../../base/image'
+import logoPic from '../../../assets/images/logo.png'
 
 const LoginForm = () => {
   return (
     <div className="width-50 flex center">
         <div className="loginCard">
+            <Image src={logoPic} alt="ParkCatch Logo" className='logoPic' />
             <Input type="text" placeholder="Email"/>
         </div>
     </div>
