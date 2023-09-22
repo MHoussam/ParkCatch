f0f34ea94680bab04edd414fbb90b1596e053e46
MHoussam <mohammadosta1999@gmail.com>
2023-09-23 01:45:47 +0300

    Client-Website <> Created style.css for the Login Page.

--- website/src/Components/ui/LoginForm/index.jsx
+++ website/src/Components/ui/LoginForm/index.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const LoginForm = () => {
   return (
     <div>
-        <div className="flex">
-            <img src="../assets/images/lot.png" alt="Parking Lot" className='parkingPic'/>
-        </div>
+        
     </div>
   )
 }
--- website/src/Pages/Login/index.jsx
+++ website/src/Pages/Login/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import LoginForm from '../../Components/ui/LoginForm'
+import lotPic from '../../assets/images/lot.png'
 
 const Login = () => {
   return (
     <div>
-        <LoginForm />
+      <div className="flex1">
+            <img src={lotPic} alt="Parking Lot" className='parkingPic'/>
+      </div>
+      <LoginForm />
     </div>
   )
 }
