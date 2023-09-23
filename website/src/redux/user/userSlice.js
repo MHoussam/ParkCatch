601f411d49387bf6871ac24aa414e3d3ba7be858
MHoussam <mohammadosta1999@gmail.com>
2023-09-23 05:17:38 +0300

    Client-Website <> Created the User Reducer in Redux.

---
+++ website/src/redux/user/userSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    firstname: "",
+    lastname: "",
+    email: "",
+    token: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+        state.firstname = action.payload.firstname,
+        state.lastname = action.payload.lastname,
+        state.email = action.payload.email
+    },
+    setUserToken: (state, action) => {
+      state.token = action.payload;
+    },
+  },
+});
+
+export const { setUser, setUserToken } = userSlice.actions;
+
+export default userSlice.reducer;
