8a5a8a3dfd9ac1acfe0d6539fce762f249b3e086
MHoussam <mohammadosta1999@gmail.com>
2023-09-23 20:51:04 +0300

    Client-Website <> Created the Style File for the Section component.

--- website/src/Components/base/section/index.jsx
+++ website/src/Components/base/section/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Image from '../image'
+import styles from './styles.css'
 
 const Section = ({ text, icon, path }) => {
   return (
     <div className='section'>
         <div className="content">
-            <Image src={icon} alt='' className={secIcon} />
+            <Image src={icon} alt='' className='secIcon' />
             <h3>{text}</h3>
         </div>
     </div>
