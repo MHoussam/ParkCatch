3e8031337fb3c0897b8a9ba98edf95efd0cc0ee2
MHoussam <mohammadosta1999@gmail.com>
2023-09-23 19:10:04 +0300

    Client-Website <> Created the Style File for the Header Component & Fixed its Style.

--- website/src/Components/ui/Header/index.jsx
+++ website/src/Components/ui/Header/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import './styles.css'
+import Image from '../../base/image'
+import headerLogo from '../../../assets/images/header-logo.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header flex align-items'>
+        <Image src={headerLogo} alt='Header Logo ParkCatch' className='header-logo'/>
+    </div>
   )
 }
 
