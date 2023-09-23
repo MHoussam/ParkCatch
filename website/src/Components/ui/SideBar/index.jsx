677e63517fe690868eb7c6ba396e4f3b33153040
MHoussam <mohammadosta1999@gmail.com>
2023-09-23 20:49:43 +0300

    Client-Website <> Added the Section Components in the SideBar Component.

--- website/src/Components/ui/SideBar/index.jsx
+++ website/src/Components/ui/SideBar/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './styles.css'
+import Section from '../../base/section'
+import terminate from '../../../assets/images/terminate.png'
+import add from '../../../assets/images/add.png'
+import remove from '../../../assets/images/remove.png'
+import logout from '../../../assets/images/logout.png'
 
 const SideBar = () => {
   return (
     <div className='sidebar flex column' >
-        <h3>Camel</h3>
-        <h3>Poirue</h3>
+        <Section icon={terminate} text='Terminate a Reservation' path='' />
+        <Section icon={add} text='Add Availability' path='' />
+        <Section icon={remove} text='Remove Availability' path='' />
+        <Section icon={logout} text='Logout' path='' />
     </div>
   )
 }
