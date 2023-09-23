6758fba0a1d7807ca1319b43babfbf28809e5658
MHoussam <mohammadosta1999@gmail.com>
2023-09-23 03:59:57 +0300

    Client-MobileApp <> Fixed the Input Component to Handle the Change of the Value.

--- website/src/Components/base/input/index.jsx
+++ website/src/Components/base/input/index.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import './styles.css'
+import React from "react";
+import "./styles.css";
 
-const Input = ({ type, placeholder }) => {
+const Input = ({ type, placeholder, value, onChange }) => {
   return (
-    <div className='width-100 flex center'>
-      <input type={type} placeholder={placeholder} className='input width-60'/>
+    <div className="width-100 flex center">
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className="input width-60"
+      />
     </div>
   );
 };
