3fcbbe24c4a6c02abcf7755c8c4e9dd26329de2c
MHoussam <mohammadosta1999@gmail.com>
2023-09-23 03:14:05 +0300

    Client-MobileApp <> Fixed the Style & Position for the LoginCard & Logo.

--- website/src/Components/ui/LoginForm/index.jsx
+++ website/src/Components/ui/LoginForm/index.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import Input from '../../base/input'
-import './styles.css'
-import Image from '../../base/image'
-import logoPic from '../../../assets/images/logo.png'
+import React from "react";
+import Input from "../../base/input";
+import "./styles.css";
+import Image from "../../base/image";
+import logoPic from "../../../assets/images/logo.png";
 
 const LoginForm = () => {
   return (
     <div className="width-50 flex center">
-        <div className="loginCard">
-            <Image src={logoPic} alt="ParkCatch Logo" className='logoPic' />
-            <Input type="text" placeholder="Email"/>
+      <div className="loginCard border flex column align-items">
+        <div className="logo flex center">
+            <Image src={logoPic} alt="ParkCatch Logo" className="logoPic" />
         </div>
+        <Input type="text" placeholder="Email" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
