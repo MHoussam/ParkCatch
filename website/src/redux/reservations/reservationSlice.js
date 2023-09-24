83e0a85fa8b539ef34894a2f7f8069a780406322
MHoussam <mohammadosta1999@gmail.com>
2023-09-24 13:49:18 +0300

    Client-Website <> Fixed the Reservation Reducer.

--- website/src/redux/reservations/reservationSlice.js
+++ website/src/redux/reservations/reservationSlice.js
@@ -1,42 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  client: null,
-  phone: '',
+  first_name: "",
+  last_name: "",
+  phone: "",
   parking: null,
   location: null,
   duration: 2,
   spotNumber: null,
-  plateNumber: '',
-  realPlateNumber: '',
+  plateNumber: "",
+  realPlateNumber: "",
   total: null,
 };
 
 const reservationSlice = createSlice({
-  name: 'reservation',
+  name: "reservation",
   initialState,
   reducers: {
     setReservation: (state, action) => {
-        state.client = action.payload.client;
-          state.phone = action.payload.phone;
-        state.parking = action.payload.parking;
-        state.location = action.payload.location;
-          state.duration = action.payload.duration;
-        state.spotNumber = action.payload.spotNumber;
-          state.plateNumber = action.payload.plateNumber;
-          state.realPlateNumber = action.payload.realPlateNumber;
-        state.total = action.payload.total;
-        },
+      state.first_name = action.payload.first_name;
+      state.last_name = action.payload.last_name;
+      state.phone = action.payload.phone;
+      state.parking = action.payload.parking;
+      state.location = action.payload.location;
+      state.duration = action.payload.duration;
+      state.spotNumber = action.payload.spotNumber;
+      state.plateNumber = action.payload.plateNumber;
+      state.realPlateNumber = action.payload.realPlateNumber;
+      state.total = action.payload.total;
+    },
     clearReservation: (state) => {
-        state.client = null;
-        state.phone = '';
-        state.parking = null;
-        state.location = null;
-        state.duration = 2;
-        state.spotNumber = null;
-        state.plateNumber = '';
-        state.realPlateNumber = '';
-        state.total = null;
+      state.first_name = "";
+      state.last_name = "";
+      state.phone = "";
+      state.parking = null;
+      state.location = null;
+      state.duration = 2;
+      state.spotNumber = null;
+      state.plateNumber = "";
+      state.realPlateNumber = "";
+      state.total = null;
     },
   },
 });
