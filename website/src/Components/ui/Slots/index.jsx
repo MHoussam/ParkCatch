a18a533393de191fb4a892ce562aa2783b5ac175
MHoussam <mohammadosta1999@gmail.com>
2023-09-24 06:57:29 +0300

    Client-Website <> Fetched the Parking's Slots & Displayed them.

--- website/src/Components/ui/Slots/index.jsx
+++ website/src/Components/ui/Slots/index.jsx
@@ -1,73 +1,74 @@
-import React, { useEffect } from 'react'
-import './styles.css'
+import React, { useEffect } from "react";
+import "./styles.css";
 
 const Slots = () => {
-    const fetchSpots = async () => {
+  const slots = useSelector((state) => state.slots);
 
-        const userToken = await localStorage.getItem('userToken');
-        console.log(userToken)
-    
-        // try {
-        //   const axiosConfig = {
-        //     headers: {
-        //       Authorization: `Bearer ${userToken}`,
-        //       "Content-Type": "application/json",
-        //     },
-        //   };
-    
-        //   const dataForm = {
-        //     parking_id: selectedParking.id,
-        //     token: userToken,
-        //   };
-        //   //  console.log(dataForm)
-        //   const response = await axios.post(
-        //     "http://127.0.0.1:8000/api/spots",
-        //     dataForm
-        //   );
+  const fetchSpots = async () => {
+    const userToken = await localStorage.getItem("userToken");
+    console.log(userToken);
 
-        //   if (Array.isArray(response.data.data)) {
-        //     if (slots.slots === null || slots.slots.length === 0) {
-        //       // console.log("here");
-        //       response.data.data.forEach((item) => {
-        //         const {
-        //           id,
-        //           parking_id,
-        //           name,
-        //           availability,
-        //           reason,
-        //           x_coordinate,
-        //           y_coordinate,
-        //           reserved,
-        //         } = item;
-    
-        //         dispatch(
-        //           addSlots({
-        //             id,
-        //             parking_id,
-        //             name,
-        //             availability,
-        //             reason,
-        //             x_coordinate,
-        //             y_coordinate,
-        //             reserved,
-        //           })
-        //         );
-        //       });
-        //       // console.log("finish");
-        //     }
-        //   } else {
-        //     console.error("Received non-array data from server:", response.data);
-        //   }
-        //   // console.log('no?')
-        // } catch (error) {
-        //   console.error("Error fetching spots data:", error);
-        //   console.log(userToken)
-        // }
+    try {
+      const axiosConfig = {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+          "Content-Type": "application/json",
+        },
       };
 
-      useEffect(() => {
-        fetchSpots();
-      }, [])
+      const dataForm = {
+        parking_id: 3,
+        token: userToken,
+      };
+      //  console.log(dataForm)
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/spots",
+        dataForm
+      );
+
+      if (Array.isArray(response.data.data)) {
+        if (slots.slots === null || slots.slots.length === 0) {
+          // console.log("here");
+          response.data.data.forEach((item) => {
+            const {
+              id,
+              parking_id,
+              name,
+              availability,
+              reason,
+              x_coordinate,
+              y_coordinate,
+              reserved,
+            } = item;
+
+            dispatch(
+              addSlots({
+                id,
+                parking_id,
+                name,
+                availability,
+                reason,
+                x_coordinate,
+                y_coordinate,
+                reserved,
+              })
+            );
+          });
+          // console.log("finish");
+        }
+      } else {
+        console.error("Received non-array data from server:", response.data);
+      }
+      // console.log('no?')
+    } catch (error) {
+      console.error("Error fetching spots data:", error);
+      console.log(userToken)
+    }
+  };
+
+  useEffect(() => {
+    fetchSpots();
+  }, []);
 
   return (
     <div className="table">
@@ -84,7 +85,29 @@ const Slots = () => {
                   : "tableCellGap flex center flex-grow"
               }
             >
-              
+              {slots.slots &&
+                slots.slots
+                  .filter(
+                    (slot) =>
+                      slot.x_coordinate === rowIndex &&
+                      slot.y_coordinate === columnIndex
+                  )
+                  .map((slot) => (
+                    <Slot
+                      key={`${rowIndex}-${columnIndex}`}
+                      number={slot.name}
+                      styleContainer={
+                        slot.reserved ? styles.reserved : styles.available
+                      }
+                      styleTitle={
+                        slot.reserved
+                          ? styles.reservedTitle
+                          : styles.availableTitle
+                      }
+                      isSelected={selectedSlot.id === slot.id}
+                      onPress={() => slotPressed(slot)}
+                    />
+                  ))}
             </div>
           ))}
         </div>
@@ -93,4 +116,4 @@ const Slots = () => {
   );
 };
 
-export default Slots
+export default Slots;
