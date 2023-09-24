48ec8e4528075fe41d73aebbcbc30f79b4412b83
MHoussam <mohammadosta1999@gmail.com>
2023-09-24 14:20:59 +0300

    Client-Website <> Added the Logic to the Handle Search Function in the SearchBar Component.

--- website/src/Components/base/searchbar/index.jsx
+++ website/src/Components/base/searchbar/index.jsx
@@ -1,17 +1,41 @@
 import React from "react";
-import './styles.css'
-import Image from "../image";
-import search from '../../../assets/images/search.png'
+import { useDispatch, useSelector } from "react-redux";
+import { setSearchFilter } from "../actions/searchActions";
 
 const SearchBar = () => {
+  const dispatch = useDispatch();
+  const [query, setQuery] = useState([]);
+  const reservation = useSelector((state) => state.reservation)
+
+  const handleSearch = (query) => {
+    const filtered = recipes.filter(
+      (reservation) =>
+        reservation.plateNumber.toLowerCase().includes(query.toLowerCase()) ||
+        reservation.realPlateNumber.toLowerCase().includes(query.toLowerCase())
+    );
+    dispatch(setSearchFilter(filtered));
+  };
+
+  const handleInputChange = (e) => {
+    const newQuery = e.target.value;
+    setQuery(newQuery);
+    handleSearch(newQuery);
+
+    if (newQuery === "") {
+      dispatch(clearSearchFilter());
+    }
+  };
+
   return (
     <div className="searchBar width-25">
-        <Image src={search} alt='' className='searchIcon' />
-        <input
+      <Image src={search} alt='' className='searchIcon' />
+      <input
         type="text"
         placeholder="Search for a Customer"
         className="search-bar-input width-95"
-        />
+        value={query}
+        onChange={handleInputChange}
+      />
     </div>
   );
 };
