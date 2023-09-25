dbf491cbd37cf3b2ba489fbd1ab1dd53e1ad13f1
MHoussam <mohammadosta1999@gmail.com>
2023-09-25 03:17:59 +0300

    Client-Website <> Created TextArea Component.

---
+++ website/src/Components/base/textArea/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './styles'
+
+const TextArea = ({ text, type, placeholder, value, state, onChange, classProp }) => {
+    const handleChange = (e) => {
+        const newValue = e.target.value;
+        onChange(newValue);
+        console.log(newValue); 
+      };
+
+  return (
+    <div className="inputContainer width-100 flex column center">
+      <h4>{text}</h4>
+      <textarea
+        rows="4"
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        className={`input width-60 ${classProp}`}
+      />
+    </div>
+  )
+}
+
+export default TextArea
