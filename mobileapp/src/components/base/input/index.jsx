1bdd7afbf6519743e5e062eaa094effe78d5e71f
MHoussam <mohammadosta1999@gmail.com>
2023-10-01 02:04:55 +0300

    Client-MobileApp <> Fixed the Handling of the Values for the Inputs.

--- mobileapp/src/components/base/input/index.jsx
+++ mobileapp/src/components/base/input/index.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text, TextInput, View } from 'react-native'
 import styles from './styles'
 import { setReservation } from '../../../redux/reservation/reservationSlice';
 import { useDispatch } from 'react-redux';
+import { DATE_FORMAT } from 'react-native-gifted-chat';
 
 const Input = ({ styleText, text, label, type }) => {
   const dispatch = useDispatch();
+  const [isPasscode, setIsPasscode] = useState(false);
 
   const handleInputChange = (value) => {
-    if(type) {
+    if(type==='number') {
       const replacedValue = value.replace(/[^0-9]/g, '');
       const correctedValue = parseInt(replacedValue, 10);
       dispatch(setReservation({ [label]: correctedValue }));
-    } else {
+    } else if(type==='text') {
       const correctedValue = value.replace(/[^a-zA-Z0-9]/g, '');
+      dispatch(setReservation({ [label]: correctedValue }));
+    } else if(type==='date') {
+      dispatch(setReservation({ [label]: value }));
+    } else if(type==='passcode') {
+      const replacedValue = value.replace(/[^0-9]/g, '');
+      const correctedValue = parseInt(replacedValue, 10);
+      setIsPasscode(true);
       dispatch(setReservation({ [label]: correctedValue }));
     }
   }
 
-  const keyboardType = type ? 'numeric' : 'default';
+  const keyboardType = type==='text' ? 'default' : 'numeric';
   const maxLength = type ? 8 : 12;
 
   return (
@@ -29,7 +38,8 @@ const Input = ({ styleText, text, label, type }) => {
         onChangeText={handleInputChange}
         maxLength={maxLength}
         keyboardType={keyboardType}
-      />
+        secureTextEntry={isPasscode}
+        />
     </View>
   )
 }
--- mobileapp/src/screens/reservationPayment/index.jsx
+++ mobileapp/src/screens/reservationPayment/index.jsx
@@ -3,6 +3,7 @@ import { View } from 'react-native'
 import Input from '../../components/base/input'
 import styles from './styles'
 import Header from '../../components/ui/header'
+import Button from '../../components/base/button'
 
 const ReservationPayment = () => {
   return (
@@ -12,8 +13,9 @@ const ReservationPayment = () => {
             <Input styleText={styles.title} text={'Cardholder Name'} label='cardholderName' type='text'/>
             <Input styleText={styles.title} text={'Card Number'} label='cardNumber' type='number'/>
             <Input styleText={styles.title} text={'Expiration Date'} label='expirationDate' type='date'/>
-            <Input styleText={styles.title} text={'CVV'} label='cvv' type='password'/>
+            <Input styleText={styles.title} text={'CVV'} label='cvv' type='passcode'/>
         </View>
+        <Button text='Pay' Navigate='ReservationTicket'/>
     </View>
   )
 }
