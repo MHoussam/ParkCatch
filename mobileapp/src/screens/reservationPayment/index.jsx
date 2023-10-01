2c6e58ab933f40f31c9c346b9f3b5b72e0ecfb34
MHoussam <mohammadosta1999@gmail.com>
2023-10-01 03:47:51 +0300

    Client-MobileApp <> Added the Total Amount to the Reservation Payment Screen.

--- mobileapp/src/screens/reservationPayment/index.jsx
+++ mobileapp/src/screens/reservationPayment/index.jsx
@@ -5,6 +5,7 @@ import styles from './styles'
 import Header from '../../components/ui/header'
 import Button from '../../components/base/button'
 import RadioButton from '../../components/base/radioButton'
+import { useSelector } from 'react-redux'
 
 const ReservationPayment = () => {
     const options = [
@@ -13,7 +14,7 @@ const ReservationPayment = () => {
       ];
 
       const [selectedOption, setSelectedOption] = useState(options[0]);
-
+      const reservation = useSelector((state) => state.reservation)
   return (
     <View style={styles.container}>
         <Header ScreenName={"Reservation Info"} />
@@ -22,7 +23,7 @@ const ReservationPayment = () => {
             <Input styleText={styles.title} text={'Card Number'} label='cardNumber' type='number' card={true}/>
             <Input styleText={styles.title} text={'Expiration Date'} label='expirationDate' type='date'/>
             <Input styleText={styles.title} text={'CVV'} label='cvv' type='passcode'/>
-            <View>
+            <View style={{marginBottom: 5}}>
                 <Text style={styles.title}>
                     Payment Method
                 </Text>
@@ -32,6 +33,14 @@ const ReservationPayment = () => {
                 onSelect={(option) => setSelectedOption(option)}
                 />
             </View>
+            <View >
+            <Text style={styles.title}>
+                Total Amount
+            </Text>
+            <Text style={{color: '#00000080', fontSize: 17, fontWeight: '600'}}>
+                {reservation.total}$
+            </Text>
+        </View>
         </View>
         <View style={styles.button}>
             <Button text='Pay' navigate='ReservationTicket'/>
