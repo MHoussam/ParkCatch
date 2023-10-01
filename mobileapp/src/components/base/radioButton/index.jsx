899310694a235e44dd1d2bb85fe9ea9d10ad18ec
MHoussam <mohammadosta1999@gmail.com>
2023-10-01 03:42:29 +0300

    Client-MobileApp <> Fixed the Style of the Radio Buttons.

--- mobileapp/src/components/base/radioButton/index.jsx
+++ mobileapp/src/components/base/radioButton/index.jsx
@@ -3,18 +3,18 @@ import { View, Text, TouchableOpacity } from "react-native";
 import styles from "./styles";
 const RadioButton = ({ options, selectedOption, onSelect }) => {
   return (
-    <View style={{ flexDirection: "row", width: '100%', marginTop: 10, }}>
+    <View style={{ flexDirection: "row", width: "100%", marginTop: 10 }}>
       {options.map((option) => (
         <TouchableOpacity
           key={option.value}
           onPress={() => onSelect(option)}
-          style={{ flexDirection: "row", alignItems: "center",}}
+          style={{ flexDirection: "row", alignItems: "center" }}
         >
-          <View style={{ marginRight: 15, flexDirection: 'row', }}>
+          <View style={{ marginRight: 15, flexDirection: "row", alignItems: 'center' }}>
             <View
               style={{
-                height: 20,
-                width: 20,
+                height: 17,
+                width: 17,
                 borderRadius: 12,
                 borderWidth: 1,
                 alignItems: "center",
@@ -25,8 +25,9 @@ const RadioButton = ({ options, selectedOption, onSelect }) => {
               {option.value === selectedOption.value && (
                 <View
                   style={{
-                    height: 20,
-                    width: 20,
+                    height: 17,
+                    borderWidth: 1,
+                    width: 17,
                     borderRadius: 12,
                     backgroundColor: "#FECA0E",
                   }}
@@ -34,7 +35,7 @@ const RadioButton = ({ options, selectedOption, onSelect }) => {
               )}
             </View>
 
-            <Text>{option.label}</Text>
+            <Text style={{fontSize: 15}}>{option.label}</Text>
           </View>
         </TouchableOpacity>
       ))}
