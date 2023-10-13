9665a9ca28dad15b08fa39ab4fa8abc8cd2a720e
MHoussam <mohammadosta1999@gmail.com>
2023-10-14 02:08:53 +0300

    Client-MobileApp <> Fixed the Fetching of the Slots.

--- mobileapp/src/components/ui/slots/index.jsx
+++ mobileapp/src/components/ui/slots/index.jsx
@@ -4,10 +4,12 @@ import styles from "./styles";
 import Slot from "../../base/slot";
 import { useDispatch, useSelector } from "react-redux";
 import { addSlots, clearSlots } from "../../../redux/slots/slotSlice";
-import { setSelectedSlot, clearSelectedSlot } from "../../../redux/selectedSlot/selectedSlotSlice";
+import {
+  setSelectedSlot,
+  clearSelectedSlot,
+} from "../../../redux/selectedSlot/selectedSlotSlice";
 import axios from "axios";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Slots = () => {
   const dispatch = useDispatch();
@@ -16,9 +18,10 @@ const Slots = () => {
   const slots = useSelector((state) => state.slots);
   const selectedSlot = useSelector((state) => state.selectedSlot);
   const reservation = useSelector((state) => state.reservation);
-  
+
   const fetchSpots = async () => {
-    const userToken = await AsyncStorage.getItem('userToken');
+    const userToken = await AsyncStorage.getItem("userToken");
+    const dispatchSlots = [];
 
     try {
       const dataForm = {
@@ -30,111 +33,93 @@ const Slots = () => {
         dataForm
       );
       if (Array.isArray(response.data.data)) {
-        if (slots.slots.length == 0) {
-          response.data.data.forEach((item) => {
-            const {
-              id,
-              parking_id,
-              name,
-              availability,
-              reason,
-              x_coordinate,
-              y_coordinate,
-              reserved,
-            } = item;
-
-            dispatch(
-              addSlots({
-                id,
-                parking_id,
-                name,
-                availability,
-                reason,
-                x_coordinate,
-                y_coordinate,
-                reserved,
-              })
-            );
-          });
-        }
+        dispatchSlots.push(...response.data.data);
       } else {
         console.error("Received non-array data from server:", response.data);
       }
     } catch (error) {
       console.error("Error fetching spots data:", error);
     }
+
+    if (dispatchSlots.length > 0) {
+      dispatch(addSlots(dispatchSlots));
+    }
   };
 
   const slotPressed = (slot) => {
-      if((selectedSlot === null || selectedSlot.id !== slot.id) && slot.reserved === false){
-        dispatch(setSelectedSlot(slot));
-      } else {
-        dispatch(clearSelectedSlot());
-      }
-  }
+    if (
+      (selectedSlot === null || selectedSlot.id !== slot.id) &&
+      slot.reserved === false
+    ) {
+      dispatch(setSelectedSlot(slot));
+    } else {
+      dispatch(clearSelectedSlot());
+    }
+  };
 
-
-  useEffect(() => {    
-      fetchSpots();
-      console.log('how')
+  useEffect(() => {
+    fetchSpots();
+    console.log("how");
   }, []);
 
-  useEffect(() => { 
+  useEffect(() => {
     return () => {
-      dispatch(clearSlots())
-    }
-    }, []);
+      dispatch(clearSlots());
+    };
+  }, []);
 
   return (
     <>
-    {slots.slots.length > 0 ? (
-      <View style={styles.table}>
-        {[...Array(12)].map((_, rowIndex) => (
-          <View key={rowIndex} style={styles.tableRow}>
-            {[1, 2, 3].map((columnIndex) => (
-              <View
-                key={columnIndex}
-                style={
-                  columnIndex !== 2
-                    ? styles.tableCell
-                    : rowIndex === 0
-                    ? styles.entranceCell
-                    : styles.tableCellGap
-                }
-              >
-                {slots.slots &&
-                  slots.slots
-                    .filter(
-                      (slot) =>
-                        slot.x_coordinate-1 === rowIndex &&
-                        slot.y_coordinate === columnIndex
-                    )
-                    .map((slot) => (
-                      <Slot
-                        key={`${rowIndex}-${columnIndex}`}
-                        number={slot.name}
-                        styleContainer={
-                          slot.reserved ? styles.reserved : styles.available
-                        }
-                        styleTitle={
-                          slot.reserved
-                            ? styles.reservedTitle
-                            : styles.availableTitle
-                        }
-                        isSelected={selectedSlot.id === slot.id}
-                        onPress={() => slotPressed(slot)}
-                      />
-                    ))}
-              </View>
-            ))}
-          </View>
-        ))}
-      </View>
-    ) : (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Slots are Loading...</Text>
-      </View>
-    )}
+      {slots.slots.length > 0 ? (
+        <View style={styles.table}>
+          {[...Array(12)].map((_, rowIndex) => (
+            <View key={rowIndex} style={styles.tableRow}>
+              {[1, 2, 3].map((columnIndex) => (
+                <View
+                  key={columnIndex}
+                  style={
+                    columnIndex !== 2
+                      ? styles.tableCell
+                      : rowIndex === 0
+                      ? styles.entranceCell
+                      : styles.tableCellGap
+                  }
+                >
+                  {slots.slots &&
+                    slots.slots
+                      .filter(
+                        (slot) =>
+                          slot.x_coordinate - 1 === rowIndex &&
+                          slot.y_coordinate === columnIndex
+                      )
+                      .map((slot) => (
+                        <Slot
+                          key={`${rowIndex}-${columnIndex}`}
+                          number={slot.name}
+                          styleContainer={
+                            slot.reserved ? styles.reserved : styles.available
+                          }
+                          styleTitle={
+                            slot.reserved
+                              ? styles.reservedTitle
+                              : styles.availableTitle
+                          }
+                          isSelected={selectedSlot.id === slot.id}
+                          onPress={() => slotPressed(slot)}
+                        />
+                      ))}
+                </View>
+              ))}
+            </View>
+          ))}
+        </View>
+      ) : (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <Text>Slots are Loading...</Text>
+        </View>
+      )}
     </>
   );
 };
--- mobileapp/src/screens/spots/index.jsx
+++ mobileapp/src/screens/spots/index.jsx
@@ -38,6 +38,7 @@ const Spots = () => {
     );
   }
 
+  console.log(slots)
   return (
     <View style={styles.container}>
       <Header ScreenName={'Spot Reservation'} />
