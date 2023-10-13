7ab4d964384135f0aa1198f5708416103ae2c2ce
MHoussam <mohammadosta1999@gmail.com>
2023-10-14 02:09:39 +0300

    Client-MobileApp <> Fixed the Fetching of the Parkings.

--- mobileapp/src/components/ui/map/index.jsx
+++ mobileapp/src/components/ui/map/index.jsx
@@ -1,24 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import { Image, Text, TouchableOpacity, View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
-import styles from './styles';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import styles from "./styles";
+import { useDispatch, useSelector } from "react-redux";
 import {
   setLocation,
   setErrorMsg,
   clearLocation,
   clearErrorMsg,
-} from '../../../redux/location/locationSlice'; 
-import { addParking, clearParkings } from '../../../redux/parking/parkingSlice';
-import axios from 'axios';
-import Distance from '../../base/distance';
-import { useNavigation } from '@react-navigation/native';
+} from "../../../redux/location/locationSlice";
+import { addParking, clearParkings } from "../../../redux/parking/parkingSlice";
+import axios from "axios";
+import Distance from "../../base/distance";
+import { useNavigation } from "@react-navigation/native";
 import { setUser, setUserToken } from "../../../redux/user/userSlice";
-import { setSelectedParking, clearSelectedParking } from "../../../redux/selectedParking/selectedParkingSlice";
-import { setSelectedSlot, clearSelectedSlot } from "../../../redux/selectedSlot/selectedSlotSlice";
-import WebSocketClient from '../../WebSocketClient';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  setSelectedParking,
+  clearSelectedParking,
+} from "../../../redux/selectedParking/selectedParkingSlice";
+import {
+  setSelectedSlot,
+  clearSelectedSlot,
+} from "../../../redux/selectedSlot/selectedSlotSlice";
+import WebSocketClient from "../../WebSocketClient";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Map = () => {
   const dispatch = useDispatch();
@@ -28,71 +34,61 @@ const Map = () => {
   const parkings = useSelector((state) => state.parking.parkings);
   const selectedParking = useSelector((state) => state.selectedParking);
   const selectedSlot = useSelector((state) => state.selectedSlot);
-  const [ availableNumber, setAvailableNumber ] = useState('');
-  const [ refresh, setRefresh ] = useState(null);
+  const [availableNumber, setAvailableNumber] = useState("");
+  const [refresh, setRefresh] = useState(null);
 
   const fetchParkings = async () => {
-    try{
-      const userData = await AsyncStorage.getItem('userData');
-      const userToken = await AsyncStorage.getItem('userToken');
-      
+    const userToken = await AsyncStorage.getItem("userToken");
+    const dispatchParkings = [];
+
+    try {
       const axiosConfig = {
         headers: {
           Authorization: `Bearer ${userToken}`,
-          'Content-Type': 'application/json'
-        }
+          "Content-Type": "application/json",
+        },
       };
-      const response = await axios.get('http://127.0.0.1:8000/api/parkings', axiosConfig);
 
-
+      const response = await axios.get(
+        "http://127.0.0.1:8000/api/parkings",
+        axiosConfig
+      );
       if (Array.isArray(response.data.data)) {
         if (parkings.length === 0) {
-          response.data.data.forEach((item) => {
-            const { id,
-              name,
-              address,
-              price,
-              photo,
-              open_hour,
-              close_hour,
-              latitude,
-              longitude, } = item;
-            dispatch(addParking({ id,
-              name,
-              address,
-              price,
-              photo,
-              open_hour,
-              close_hour, 
-              latitude: parseFloat(latitude), 
-              longitude: parseFloat(longitude) }));
-          });
-          await AsyncStorage.setItem('parkings', JSON.stringify(response.data.data));
+          dispatchParkings.push(...response.data.data);
         }
       } else {
-        console.error('Received non-array data from server:', response.data);
+        console.error(
+          "Received non-array data from the server:",
+          response.data
+        );
       }
-    } catch(error) {
-      console.error('Error fetching parking data:', error);
+    } catch (error) {
+      console.error("Error fetching parking data:", error);
     }
-  }
+
+    if (dispatchParkings.length > 0 && parkings.length == 0) {
+      await AsyncStorage.setItem("parkings", JSON.stringify(dispatchParkings));
+      dispatch(addParking(dispatchParkings));
+    }
+  };
 
   const fetchMap = async () => {
-      const response = await Location.requestForegroundPermissionsAsync();
+    const response = await Location.requestForegroundPermissionsAsync();
 
-      if (response.status !== 'granted') {
-        dispatch(setErrorMsg('Permission to access location was denied'));
-        await Location.requestForegroundPermissionsAsync();
-        dispatch(clearLocation());
-      }
+    if (response.status !== "granted") {
+      dispatch(setErrorMsg("Permission to access location was denied"));
+      await Location.requestForegroundPermissionsAsync();
+      dispatch(clearLocation());
+    }
 
-      const location = await Location.getCurrentPositionAsync({});
-      dispatch(setLocation(location));
-      dispatch(clearErrorMsg());
-  }
+    const location = await Location.getCurrentPositionAsync({});
+    dispatch(setLocation(location));
+    dispatch(clearErrorMsg());
+  };
 
   const handleMarkerPress = async (parking) => {
-    dispatch(setSelectedParking(parking)); 
+    dispatch(setSelectedParking(parking));
   };
 
   const closeCard = () => {
@@ -100,30 +96,33 @@ const Map = () => {
   };
 
   const navigateToSpots = () => {
-    navigation.navigate('Spots')
+    navigation.navigate("Spots");
   };
 
   const fetchAvailableNumber = async () => {
     try {
-      const userToken = await AsyncStorage.getItem('userToken');
+      const userToken = await AsyncStorage.getItem("userToken");
 
       const dataForm = {
         token: userToken,
         parking_id: selectedParking.id,
       };
-  
-      const response = await axios.post('http://127.0.0.1:8000/api/availableSpots', dataForm);
-  
+
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/availableSpots",
+        dataForm
+      );
+
       setAvailableNumber(response.data.data);
     } catch (error) {
-      console.log('Error while fetching the available spots number: ' + error);
+      console.log("Error while fetching the available spots number: " + error);
     }
-  }
+  };
 
   const cardPress = () => {
     dispatch(clearSelectedSlot());
     navigateToSpots();
-  }
+  };
 
   const calculatedDistance = location
     ? Distance({
@@ -134,9 +133,9 @@ const Map = () => {
       })
     : null;
 
-    const refreshNow = () => {
-      setRefresh(true);
-    }
+  const refreshNow = () => {
+    setRefresh(true);
+  };
 
   useEffect(() => {
     if (selectedParking && selectedParking.id) {
@@ -147,13 +146,13 @@ const Map = () => {
   }, [selectedParking]);
 
   const checkParkings = async () => {
-    const park = await AsyncStorage.getItem('parkings')
+    const park = await AsyncStorage.getItem("parkings");
     const parkings = JSON.parse(park);
 
     if (parkings === null) {
       checkParkings();
     }
-  }
+  };
 
   useEffect(() => {
     fetchMap();
@@ -165,85 +164,110 @@ const Map = () => {
       {errorMsg ? (
         <Text style={styles.error}>{errorMsg}</Text>
       ) : location ? (
-      <>
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-          onPress={() => {
-            if (selectedParking) {
-              closeCard();
-            }
-          }}
-          zoomEnabled={true}
-        >
-          <Marker
-            coordinate={{
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
-            }}
-            title="Your Location"
-          />
-          {parkings.map((parking) => (
-            <Marker
-              key={parking.id}
-              coordinate={{
-                latitude: parking.latitude,
-                longitude: parking.longitude,
-              }}
-              onPress={() => handleMarkerPress(parking)}
-            >
-              <View style={styles.parking}>
-                <Image
-                  source={require('../../../../assets/images/marker.png')}
-                  style={styles.markerIcon}
-                />
-                <Text style={styles.parkingName}>{parking.name}</Text>
-              </View>
-            </Marker>
-          ))}
-        </MapView>
-        <View>
-          {selectedParking.id !== null && (
+        <>
+          {parkings.length > 0 ? (
             <>
-              <TouchableOpacity style={styles.card} onPress={cardPress}>
-              <Image
-                style={styles.parkingPhoto}
-                source={{
-                  uri: `http://127.0.0.1:8000/images/parkings/${selectedParking.photo}`,
+              <MapView
+                style={styles.map}
+                initialRegion={{
+                  latitude: location.coords.latitude,
+                  longitude: location.coords.longitude,
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
                 }}
-              />
-              <View >
-                <View style={styles.cardInfo} >
-                  <Text style={[styles.bold, styles.size16]}>{selectedParking.name}</Text>
-                  <Text style={[styles.bold, styles.size13, styles.parkingAddress]}>{selectedParking.address}</Text>
-                  <View style={styles.cardInfoRow} >
-                    <View style={styles.cardInfoRow}>
-                      <Image source={require('../../../../assets/images/spots.png')} />
-                      <Text style={[styles.semiBold, styles.size13]}> {availableNumber} Spots</Text>
+                onPress={() => {
+                  if (selectedParking) {
+                    closeCard();
+                  }
+                }}
+                zoomEnabled={true}
+              >
+                <Marker
+                  coordinate={{
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                  }}
+                  title="Your Location"
+                />
+                {parkings.map((parking) => (
+                  <Marker
+                    key={parking.id}
+                    coordinate={{
+                      latitude: parseFloat(parking.latitude),
+                      longitude: parseFloat(parking.longitude),
+                    }}
+                    onPress={() => handleMarkerPress(parking)}
+                  >
+                    <View style={styles.parking}>
+                      <Image
+                        source={require("../../../../assets/images/marker.png")}
+                        style={styles.markerIcon}
+                      />
+                      <Text style={styles.parkingName}>{parking.name}</Text>
                     </View>
-                    <View style={styles.cardInfoRow}>
-                      <Image source={require('../../../../assets/images/distance.png')} />
-                      <Text style={[styles.semiBold, styles.size13]}>
-                        {calculatedDistance}m
-                      </Text>
-                    </View>
-                  </View>
-                  <Text style={styles.medium}>${selectedParking.price}/hr</Text>
-                </View>
+                  </Marker>
+                ))}
+              </MapView>
+              <View>
+                {selectedParking.id !== null && (
+                  <>
+                    <TouchableOpacity style={styles.card} onPress={cardPress}>
+                      <Image
+                        style={styles.parkingPhoto}
+                        source={{
+                          uri: `http://127.0.0.1:8000/images/parkings/${selectedParking.photo}`,
+                        }}
+                      />
+                      <View>
+                        <View style={styles.cardInfo}>
+                          <Text style={[styles.bold, styles.size16]}>
+                            {selectedParking.name}
+                          </Text>
+                          <Text
+                            style={[
+                              styles.bold,
+                              styles.size13,
+                              styles.parkingAddress,
+                            ]}
+                          >
+                            {selectedParking.address}
+                          </Text>
+                          <View style={styles.cardInfoRow}>
+                            <View style={styles.cardInfoRow}>
+                              <Image
+                                source={require("../../../../assets/images/spots.png")}
+                              />
+                              <Text style={[styles.semiBold, styles.size13]}>
+                                {" "}
+                                {availableNumber} Spots
+                              </Text>
+                            </View>
+                            <View style={styles.cardInfoRow}>
+                              <Image
+                                source={require("../../../../assets/images/distance.png")}
+                              />
+                              <Text style={[styles.semiBold, styles.size13]}>
+                                {calculatedDistance}m
+                              </Text>
+                            </View>
+                          </View>
+                          <Text style={styles.medium}>
+                            ${selectedParking.price}/hr
+                          </Text>
+                        </View>
+                      </View>
+                    </TouchableOpacity>
+                  </>
+                )}
               </View>
-              </TouchableOpacity>
             </>
-          )}
-        </View>
-      </>
+          ) : (<></>)}
+        </>
       ) : (
         <>
-          <Text style={styles.error} onLayout={refreshNow}>Map is Loading...</Text>
+          <Text style={styles.error} onLayout={refreshNow}>
+            Map is Loading...
+          </Text>
         </>
       )}
     </View>
